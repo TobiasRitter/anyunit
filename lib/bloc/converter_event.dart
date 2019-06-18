58fa0077d8eb9da01b48d685edda55c1c3110f92
Tobias <tobias.ritter98@gmail.com>
2019-06-17 22:18:29 -0400

    create events

--- lib/bloc/converter_event.dart
+++ lib/bloc/converter_event.dart
@@ -1,7 +1,17 @@
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 
+/// abstract class for a converter event
 @immutable
 abstract class ConverterEvent extends Equatable {
   ConverterEvent([List props = const []]) : super(props);
 }
+
+/// gets raised whenever the unit is changed
+class UnitChangedEvent extends ConverterEvent {}
+
+/// gets raised whenever the value is changed
+class ValueChangedEvent extends ConverterEvent {}
+
+/// gets raised whenever the convert button is pressed
+class ConvertButtonPressedEvent extends ConverterEvent {}
