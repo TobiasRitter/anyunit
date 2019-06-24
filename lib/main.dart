012c8cdc2bd94049c09d3f67a68e1c239e176049
Tobias <tobias.ritter98@gmail.com>
2019-06-24 18:15:27 -0400

    remove debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -7,22 +7,21 @@ void main() => runApp(CleverConvertApp());
 class CleverConvertApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    bool light = true;
     return MaterialApp(
       title: 'CleverConvert',
-      theme: light
-          ? ThemeData(
-              brightness: Brightness.light,
-              primaryColor: Colors.white,
-              canvasColor: Colors.white,
-              accentColor: Colors.greenAccent,
-            )
-          : ThemeData(
-              brightness: Brightness.dark,
-              primaryColor: Colors.black,
-              canvasColor: Colors.black,
-              accentColor: Colors.greenAccent,
-            ),
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primaryColor: Colors.white,
+        canvasColor: Colors.white,
+        accentColor: Colors.greenAccent,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.black,
+        canvasColor: Colors.black,
+        accentColor: Colors.greenAccent,
+      ),
       home: InputScreen(),
     );
   }
