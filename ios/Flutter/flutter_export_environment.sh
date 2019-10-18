0fb2b515ccc4550ebae774fb53011a8a944d7ee5
Tobias Ritter <tobias.ritter98@gmail.com>
2019-10-18 12:11:01 +0200

    update flutter path

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/tobias/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/tobias/DATA/Documents/Programmieren/Flutter/flutterProjects/cleverconvert"
+export "FLUTTER_APPLICATION_PATH=/mnt/DATA/Documents/Programmieren/Flutter/flutterProjects/cleverconvert"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
